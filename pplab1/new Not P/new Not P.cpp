58935ce370e839f516db26ff30b1bef710909b85
jehad-abdallah <gehad.abdallah30194@gmail.com>
2019-06-14 00:39:03 +0300

    Add files via upload

---
+++ pplab1/new Not P/new Not P.cpp
@@ -0,0 +1,173 @@
+﻿#include "pch.h"
+#include <conio.h>
+#include <windows.h>
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+
+using namespace std;
+
+int threadCount = 1;
+
+struct Argumets {
+	vector<vector<int>>* origin;
+	vector<vector<int>>* result;
+	int start;
+	int end;
+	int determinant;
+};
+
+vector<vector<int>> readMatrix() {
+	ifstream fin("input.txt");
+	int n;
+	fin >> n;
+	vector<vector<int>> matrix(n, vector<int>(n));
+	for (int y = 0; y < n; y++) {
+		for (int x = 0; x < n; x++) {
+			fin >> matrix[y][x];
+		}
+	}
+	return matrix;
+}
+
+int findDeterminant(vector<vector<int>> matrix) {
+	int res = 0;
+	int mult = 1;
+	if (matrix.size() < 1)
+	{
+		return 0;
+	}
+	if (matrix.size() == 1)
+	{
+		res = matrix[0][0];
+	}
+	else if (matrix.size() == 2) {
+		res = matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1];
+	}
+	else
+	{
+		for (int x = 0; x < matrix.size(); x++) {
+			int diagonal = 1;
+			for (int y = 0; y < matrix.size(); y++) {
+				diagonal *= matrix[y][(x + y) % matrix.size()];
+			}
+			res += diagonal;
+
+			diagonal = 1;
+			for (int y = 0; y < matrix.size(); y++) {
+				diagonal *= matrix[y][(matrix.size() - y + x) % matrix.size()];
+			}
+
+			res -= diagonal;
+		}
+	}
+	return res;
+}
+
+vector<vector<int>> getMinor(vector<vector<int>>* matrixP, int row, int col) {
+	auto matrix = *matrixP;
+	vector<vector<int>> minor(matrix.size() - 1, vector<int>(matrix.size() - 1));
+	bool isYpassed = false;
+	bool isXpassed = false;
+
+	for (int x = 0; x < matrix.size(); x++) {
+		if (x == col) {
+			isXpassed = true;
+			continue;
+		}
+		for (int y = 0; y < matrix.size(); y++) {
+			if (y == row) {
+				isYpassed = true;
+				continue;
+			}
+			minor[y - (isYpassed ? 1 : 0)][x - (isXpassed ? 1 : 0)] = matrix[y][x];
+		}
+		isYpassed = false;
+	}
+	return minor;
+}
+
+int findAlgebraicAddition(vector<vector<int>>* matrixP, int row, int col) {
+	return findDeterminant(getMinor(matrixP, row, col)) * ((row + col) % 2 == 1 ? -1 : 1);
+}
+
+DWORD WINAPI ThreadFunc(PVOID pvParam) {
+	Argumets *matrixDescriptorP = (Argumets*)pvParam;
+	std::vector<std::vector<int>> matrix;
+	int det = findDeterminant(matrix);
+	for (int col = matrixDescriptorP->start; col < matrixDescriptorP->end; col++) {
+		for (int x = 0; x < matrixDescriptorP->origin->size(); x++) {
+			(*(matrixDescriptorP->result))[x][col] = findAlgebraicAddition(matrixDescriptorP->origin, col, x) / matrixDescriptorP->determinant;
+		}
+	}
+	ExitThread(0);
+}
+
+vector<vector<int>> getInverseMatrix(vector<vector<int>> matrix) {
+	vector<vector<int>> back(matrix.size(), vector<int>(matrix.size()));
+
+	int determinant = findDeterminant(matrix);
+
+	if (determinant < 0.05 && determinant > -0.05) {
+		cout << "Determinant is equal to zero" << endl;
+		return back;
+	}
+
+	if (matrix.size() < threadCount) {
+		threadCount = matrix.size();
+	}
+
+	vector<int> bounds(threadCount + 1);
+	int step = matrix.size() / threadCount;
+	int currentStep = 0;
+
+	for (int i = 1; i <= threadCount; i++) {
+		currentStep += step;
+		bounds[i] = currentStep;
+	}
+	bounds[bounds.size() - 1] = matrix.size();
+	int indexThread = 0;
+	HANDLE* handles = new HANDLE[threadCount];
+
+	for (int row = 1; row < bounds.size(); row++) {
+		// Запуск в отдельном потоке
+		if (indexThread == threadCount) {
+			WaitForMultipleObjects(threadCount, handles, TRUE, INFINITE);
+			indexThread = 0;
+		}
+		Argumets* matrixDescriptor = new Argumets;
+		matrixDescriptor->origin = &matrix;
+		matrixDescriptor->result = &back;
+		matrixDescriptor->start = bounds[row - 1];
+		matrixDescriptor->end = bounds[row];
+		matrixDescriptor->determinant = determinant;
+		LPVOID matrixDescriptorThP = (LPVOID)matrixDescriptor;
+		handles[row - 1] = CreateThread(NULL, 4096, &ThreadFunc, matrixDescriptorThP, 0, NULL);
+	}
+
+	WaitForMultipleObjects(threadCount, handles, true, INFINITE);
+
+	return back;
+}
+
+
+int main(int argc, const char * argv[]) {
+	if (argc < 2) {
+		std::cout << "invalid arguments\n";
+	}
+	unsigned long start_time = clock();
+	threadCount = std::stod(argv[1]);
+	vector<vector<int>> matrix = readMatrix();
+	vector<vector<int>> result = getInverseMatrix(matrix);
+
+	unsigned long end_time = clock();
+	unsigned long search_time = end_time - start_time;
+	cout << "Done" << endl;
+	cout << search_time << " ms." << endl;
+
+
+	return 0;
+}
---
+++ pplab1/pp/pp.cpp
@@ -0,0 +1,70 @@
+﻿// pp.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <stdio.h>
+#include <conio.h>
+#include <windows.h> 
+#include "time.h"
+#include <iostream>
+using namespace std;
+const int n = 3;
+int a[n][n] = { 1, -2, 0, 4, 6, 2, -3, 4, -2 };
+int b[n][n] = { 0, 2, 0, 1, 1, 1, 5, -3, 10 };
+int c[n][n];
+
+DWORD WINAPI ThreadFunc(PVOID pvParam)
+{
+	int num, i, j;
+	num = (*((int *)pvParam));
+	for (i = 0; i < n; i++)
+	{
+		c[num][i] = 0;
+		for (j = 0; j < n; j++)
+			c[num][i] += a[num][j] * b[j][i];
+	}
+
+	DWORD dwResult = 0;
+	return dwResult;
+}
+
+int main()
+{
+	int i, j;
+	int x[n];
+
+	DWORD dwThreadId[n - 1], dw;
+	HANDLE hThread[n - 1];
+
+	for (i = 1; i < n; i++)
+	{
+		x[i] = i;
+		hThread[i - 1] = CreateThread(NULL, 0, ThreadFunc, (PVOID)&x[i], 0, &dwThreadId[i - 1]);
+		if (!hThread) printf("main process: thread %d not execute!", i);
+	}
+
+	x[0] = 0;
+	ThreadFunc((PVOID)&x[0]);
+
+	dw = WaitForMultipleObjects(n - 1, hThread, TRUE, INFINITE);
+
+	for (i = 0; i < n; i++)
+	{
+		fprintf(stdout, "\n");
+		for (j = 0; j < n; j++) printf("%d ", c[i][j]);
+	}
+	int t = clock();
+	cout << clock() - t << " ms" << endl;
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
